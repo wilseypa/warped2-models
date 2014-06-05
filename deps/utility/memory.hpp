8c740a2201470eb92166985b5cd7884d7da6779e
AJ Alt <aj@alt>
2014-06-05 17:36:28 +0100

    Added utilities from warped

---
+++ deps/utility/memory.hpp
@@ -0,0 +1,15 @@
+#ifndef WARPED_MODELS_UTILITY_MEMORY_HPP
+#define WARPED_MODELS_UTILITY_MEMORY_HPP
+
+#include <memory>
+#include <utility>
+
+// This is a back-port of C++14's std::make_unique<> to C++11 taken from from
+// http://herbsutter.com/gotw/_102/. Once C++14 becomes standard, remove this
+// function and replace any usage of it with the std version.
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique(Args&& ...args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+#endif
---
+++ deps/utility/warnings.hpp
@@ -0,0 +1,7 @@
+#ifndef WARPED_MODELS_UTILITY_WARNINGS_HPP
+#define WARPED_MODELS_UTILITY_WARNINGS_HPP
+
+// This template will silence unused parameter warnings.
+template<class... T> void unused(T&& ...) {}
+
+#endif
