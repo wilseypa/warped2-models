794cd0c5f4209034d49456b38c200689c0792ed0
ecarver <carver@ecarver.tk>
2014-08-13 22:07:31 -0400

    Implemented event handler for airports with unlimited runway space

--- models/airport/airport.cpp
+++ models/airport/airport.cpp
@@ -24,6 +24,11 @@ virtual std::vector<std::unique_ptr<warped::Event> > Airport::createInitialEvent
   return events;
 }
 
+static inline std::string Airport::object_name(const unsigned int object_index)
+{
+  return std::string("Object ") + std::to_string(object_index);
+}
+
 virtual std::vector<std::unique_ptr<warped::Event> > Airport::receiveEvent(const warped::Event& event)
 {
   std::vector<std::unique_ptr<warped::Event> > response_events;
@@ -31,9 +36,39 @@ virtual std::vector<std::unique_ptr<warped::Event> > Airport::receiveEvent(const
 
   std::exponential_distribution<unsigned int> depart_expo(this->depart_mean);
   std::exponential_distribution<unsigned int> land_expo(this->land_mean);
+  // std::uniform_int_distribution<unsigned int> rand_direction(0,3);
+  std::uniform_int_distribution<unsigned int> rand_airport(0,this->num_airports-1);
 
   switch ( received_event.type ) {
-  case DEPARTURE: {
-    this->state.grounded_planes--;
-    unsigned int landing_time = received_event.ts + land_expo(this->rng_engine);
-    
+  case DEPARTURE:
+    {
+      this->state.planes_grounded--;
+      this->state.planes_flying++;
+      this->state.departures++;
+      // Schedule an arrival at a random airport
+      unsigned int landing_time = received_event.ts + land_expo(this->rng_engine);
+      // direction_t direction = (direction_t) rand_direction(this->rng_engine);
+      unsigned int destination_index = rand_airport(this->rng_engine);
+      response_events.emplace_back(new AirportEvent {Airport::object_name(destination_index), this->name,
+            ARRIVAL, landing_time });
+      break;
+    }
+  case ARRIVAL:
+    {
+      // Schedule a landing
+      response_events.emplace_back(new AirportEvent { this->name, this->name, LANDING, received_event.ts });
+      break;
+    }
+  case LANDING:
+    {
+      this->state.landings++;
+      this->state.planes_flying--;
+      this->state.planes_grounded++;
+      // Schedule a departure
+      response_events.emplace_back(new AirportEvent { this->name, this->name, DEPARTURE,
+            received_event.ts + depart_expo(this->rng_engine) });
+      break;
+    }
+  }
+  return response_events;
+}
--- models/airport/airport.hpp
+++ models/airport/airport.hpp
@@ -13,10 +13,9 @@
 
 WARPED_DEFINE_OBJECT_STATE_STRUCT(AirportState) {
   unsigned int landings;
+  unsigned int departures;
   unsigned int planes_flying;
   unsigned int planes_grounded;
-  unsigned int wait_time;
-  unsigned int furthest_flight_landing;
 };
 
 enum airport_event_t {
@@ -25,15 +24,19 @@ enum airport_event_t {
   LANDING
 };
 
+enum direction_t {
+  NORTH,
+  EAST,
+  SOUTH,
+  WEST
+};
+
 class AirportEvent : public warped::Event {
 public:
   AirportEvent() = default;
   AirportEvent(const std::string& receiver_name, const std::string& creator_name,
-               const airport_event_t type, const unsigned int timestamp,
-               const unsigned int waiting_time, const unsigned int saved_furthest_flight_landing)
-    : receiver_name(receiver_name), creator_name(creator_name), type(type),
-      ts(timestamp), waiting_time(waiting_time),
-      saved_furthest_flight_landing(saved_furthest_flight_landing) {}
+               const airport_event_t type, const unsigned int timestamp)
+    : receiver_name(receiver_name), creator_name(creator_name), type(type), ts(timestamp) {}
 
   const std::string& receiverName() const { return receiver_name; }
   unsigned int timestamp() const { return ts; }
@@ -42,19 +45,16 @@ public:
   std::string creator_name;
   airport_event_t type;
   unsigned int ts;
-  unsigned int waiting_time;
-  unsigned int saved_furthest_flight_landing;
 
-  WARPED_REGISTER_SERIALIZABLE_MEMBERS(receiver_name, creator_name, type, ts, waiting_time,
-                                       saved_furthest_flight_landing)
+  WARPED_REGISTER_SERIALIZABLE_MEMBERS(receiver_name, creator_name, type, ts);
 };
 
 class Airport : public warped::SimulationObject {
 public:
   Airport(const std::string& name, const unsigned int num_airports, const unsigned int num_planes,
-          const unsigned int land_mean, const unsigned int depart_mean)
+          const unsigned int land_mean, const unsigned int depart_mean, const unsigned int index)
     : SimulationObject(name), state(), num_airports(num_airports), num_planes(num_planes),
-      land_mean(land_mean), depart_mean(depart_mean) {}
+      land_mean(land_mean), depart_mean(depart_mean), index(index) {}
 
   virtual std::vector<std::unique_ptr<warped::Event> > createInitialEvents();
   virtual std::vector<std::unique_ptr<warped::Event> > receiveEvent(const warped::Event&);
@@ -62,12 +62,15 @@ public:
 
   AirportState state;
 
+  static inline std::string object_name(const unsigned int);
+
 protected:
   std::default_random_engine rng_engine;
   const unsigned int num_airports;
   const unsigned int num_planes;
   const unsigned int land_mean;
   const unsigned int depart_mean;
+  const unsigned int index;
 };
 
 #endif
