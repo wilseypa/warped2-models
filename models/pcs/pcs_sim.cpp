adc56fe40d062fa23f33c58ee6b34ee5ab04a546
Sounak Gupta <guptask@mail.uc.edu>
2015-04-01 08:19:43 -0400

    pcs state working for all kernel configurations. restoreState problem fixed.

--- models/pcs/pcs_sim.cpp
+++ models/pcs/pcs_sim.cpp
@@ -15,18 +15,17 @@
 
 #include "tclap/ValueArg.h"
 
-#define NEG_EXPL_OFFSET 10
+#define NEG_EXPL_OFFSET  1
 
 WARPED_REGISTER_POLYMORPHIC_SERIALIZABLE_CLASS(PcsEvent)
 
 std::vector<std::shared_ptr<warped::Event> > PcsCell::createInitialEvents() {
 
+    NegativeExpntl interval_expo(call_interval_mean_, rng_.get());
     std::vector<std::shared_ptr<warped::Event>> events;
-    for (auto it = state_->portables_->begin(); it != state_->portables_->end(); it++) {
-        auto portable = it->second;
-        assert(portable != nullptr);
-        events.emplace_back(new PcsEvent {this->name_, portable->call_interval_, 
-                                                            portable, CALL_ARRIVAL});
+    for (unsigned int i = 0; i < portable_init_cnt_; i++) {
+        auto interval = (unsigned int) interval_expo();
+        events.emplace_back(new PcsEvent {this->name_, interval, 0, CALL_ARRIVAL});
     }
     return events;
 }
@@ -36,97 +35,68 @@ std::vector<std::shared_ptr<warped::Event> > PcsCell::receiveEvent(const warped:
     std::vector<std::shared_ptr<warped::Event>> events;
     auto pcs_event = static_cast<const PcsEvent&>(event);
     auto timestamp = pcs_event.event_timestamp_;
-    auto pid       = pcs_event.pid_;
+
+    NegativeExpntl interval_expo(call_interval_mean_, rng_.get());
+    NegativeExpntl duration_expo(call_duration_mean_, rng_.get());
+    NegativeExpntl move_expo(move_interval_mean_, rng_.get());
 
     switch (pcs_event.event_type_) {
 
         case CALL_ARRIVAL: {
 
-            auto it = state_->portables_->find(pid);
-            assert(it != state_->portables_->end());
-            auto portable = it->second;
-            assert(portable != nullptr);
-
-            if (state_->idle_channel_cnt_) {
-
-                state_->idle_channel_cnt_--;
-                portable->is_busy_ = true;
-                portable->call_arrival_ts_ = timestamp;
-
-                NegativeExpntl move_expo(pcs_event.call_duration_, this->rng_.get());
+            if (state_.idle_channel_cnt_) {
+                state_.idle_channel_cnt_--;
                 auto move_interval = (unsigned int) move_expo() + NEG_EXPL_OFFSET;
-                if (move_interval < portable->call_duration_) {
+                auto call_duration = (unsigned int) duration_expo() + NEG_EXPL_OFFSET;
+
+                if (move_interval < call_duration) {
                     events.emplace_back(new PcsEvent {name_, timestamp + move_interval, 
-                                                                    portable, PORTABLE_MOVE_OUT});
+                                                            timestamp, PORTABLE_MOVE_OUT});
                 } else {
-                    events.emplace_back(new PcsEvent {name_, timestamp + portable->call_duration_, 
-                                                                    portable, CALL_COMPLETION});
+                    events.emplace_back(new PcsEvent {name_, timestamp + call_duration, 
+                                                                        0, CALL_COMPLETION});
                 }
             } else {
-                events.emplace_back(new PcsEvent {name_, timestamp + portable->call_interval_, 
-                                                                    portable, CALL_ARRIVAL});
+                auto call_interval = (unsigned int) interval_expo() + NEG_EXPL_OFFSET;
+                events.emplace_back(new PcsEvent {name_, timestamp + call_interval, 
+                                                                        0, CALL_ARRIVAL});
             }
         } break;
 
         case CALL_COMPLETION: {
 
-            auto it = state_->portables_->find(pid);
-            assert(it != state_->portables_->end());
-            auto portable = it->second;
-            assert(portable != nullptr);
-
-            state_->idle_channel_cnt_++;
-            portable->is_busy_ = false;
-            events.emplace_back(new PcsEvent {name_, timestamp + portable->call_interval_, 
-                                                                    portable, CALL_ARRIVAL});
+            state_.idle_channel_cnt_++;
+            auto call_interval = (unsigned int) interval_expo() + NEG_EXPL_OFFSET;
+            events.emplace_back(new PcsEvent {name_, timestamp + call_interval, 0, CALL_ARRIVAL});
         } break;
 
         case PORTABLE_MOVE_OUT: {
 
-            auto it = state_->portables_->find(pid);
-            assert(it != state_->portables_->end());
-            auto portable = it->second;
-            assert(portable != nullptr);
-
-            state_->portables_->erase(pid);
-            state_->idle_channel_cnt_++;
-            events.emplace_back(new PcsEvent {random_move(), timestamp + NEG_EXPL_OFFSET, 
-                                                                portable, PORTABLE_MOVE_IN});
-            std::move(portable);
-
+            state_.idle_channel_cnt_++;
+            events.emplace_back(new PcsEvent {random_move(), timestamp, 
+                                            pcs_event.call_arrival_ts_, PORTABLE_MOVE_IN});
         } break;
 
         case PORTABLE_MOVE_IN: {
 
-            auto it = state_->portables_->find(pid);
-            assert(it == state_->portables_->end());
-
-            auto portable = std::make_shared<Portable> (pid, 
-                                                        pcs_event.is_busy_, 
-                                                        pcs_event.call_arrival_ts_, 
-                                                        pcs_event.call_interval_, 
-                                                        pcs_event.call_duration_);
-            assert(portable != nullptr);
-            state_->portables_->insert(state_->portables_->begin(), 
-                std::pair <unsigned int, std::shared_ptr<Portable>> (portable->pid_, portable));
-
             bool call_terminated = true;
-            if (portable->is_busy_ && state_->idle_channel_cnt_) {
-                auto completion_timestamp = pcs_event.call_arrival_ts_ + pcs_event.call_duration_;
+            if (pcs_event.call_arrival_ts_ && state_.idle_channel_cnt_) {
+
+                auto call_duration = (unsigned int) duration_expo() + NEG_EXPL_OFFSET;
+                auto completion_timestamp = pcs_event.call_arrival_ts_ + call_duration;
+
                 if (completion_timestamp > timestamp) {
-                    state_->idle_channel_cnt_--;
+                    state_.idle_channel_cnt_--;
                     events.emplace_back(new PcsEvent {name_, completion_timestamp, 
-                                                                    portable, CALL_COMPLETION});
+                                                                        0, CALL_COMPLETION});
                     call_terminated = false;
                 }
             }
 
             if (call_terminated) {
-                portable->is_busy_ = false;
-                assert((*state_->portables_)[pid] != nullptr);
-                events.emplace_back(new PcsEvent {name_, 
-                                        timestamp + (*state_->portables_)[pid]->call_interval_, 
-                                                        (*state_->portables_)[pid], CALL_ARRIVAL});
+                auto call_interval = (unsigned int) interval_expo() + NEG_EXPL_OFFSET;
+                events.emplace_back(new PcsEvent {name_, timestamp + call_interval, 
+                                                                        0, CALL_ARRIVAL});
             }
         } break;
 
@@ -139,14 +109,14 @@ std::vector<std::shared_ptr<warped::Event> > PcsCell::receiveEvent(const warped:
 
 std::string PcsCell::compute_move(direction_t direction) {
 
-    int new_x = 0, new_y = 0;
-    int current_y = ((int)this->index_) / num_cells_x_;
-    int current_x = ((int)this->index_) - (current_y * num_cells_x_);
+    unsigned int new_x = 0, new_y = 0;
+    unsigned int current_y = index_ / num_cells_x_;
+    unsigned int current_x = index_ % num_cells_x_;
 
     switch (direction) {
 
         case LEFT: {
-            new_x = ((current_x - 1) + num_cells_x_) % num_cells_x_;
+            new_x = (current_x + num_cells_x_ - 1) % num_cells_x_;
             new_y = current_y;
         } break;
 
@@ -157,7 +127,7 @@ std::string PcsCell::compute_move(direction_t direction) {
 
         case DOWN: {
             new_x = current_x;
-            new_y = ((current_y - 1) + num_cells_y_) % num_cells_y_;
+            new_y = (current_y + num_cells_y_ - 1) % num_cells_y_;
         } break;
 
         case UP: {
@@ -171,7 +141,7 @@ std::string PcsCell::compute_move(direction_t direction) {
         }
     }
 
-    return std::string("Object ") + std::to_string(new_x + (new_y * num_cells_x_));
+    return std::string("Cell_") + std::to_string(new_x + (new_y * num_cells_x_));
 }
 
 std::string PcsCell::random_move() {
@@ -185,8 +155,9 @@ int main(int argc, const char **argv) {
 
     unsigned int num_cells_x        = 100;
     unsigned int num_cells_y        = 100;
-    unsigned int call_interval_mean = 500;
+    unsigned int call_interval_mean = 400;
     unsigned int call_duration_mean = 250;
+    unsigned int move_interval_mean = 500;
     unsigned int channel_cnt        = 10;
     unsigned int num_portables      = 25;
 
@@ -199,14 +170,21 @@ int main(int argc, const char **argv) {
                                                     false, call_interval_mean, "unsigned int");
     TCLAP::ValueArg<unsigned int> call_duration_mean_arg("d", "call-duration", "Mean call duration", 
                                                     false, call_duration_mean, "unsigned int");
+    TCLAP::ValueArg<unsigned int> move_interval_mean_arg("m", "move-interval", 
+                                "Mean time between end of last move and next move", 
+                                                    false, move_interval_mean, "unsigned int");
     TCLAP::ValueArg<unsigned int> channel_cnt_arg("n", "channel-count",
                         "Number of communication channels", false, channel_cnt, "unsigned int");
     TCLAP::ValueArg<unsigned int> num_portables_arg("p", "portable-count", 
-                            "Portables per cell", false, num_portables, "unsigned int");
+                                "Portables per cell", false, num_portables, "unsigned int");
 
-    std::vector<TCLAP::Arg*> cmd_line_args = {&num_cells_x_arg, &num_cells_y_arg, 
-                                                &call_interval_mean_arg, &call_duration_mean_arg, 
-                                                &channel_cnt_arg, &num_portables_arg};
+    std::vector<TCLAP::Arg*> cmd_line_args = {  &num_cells_x_arg, 
+                                                &num_cells_y_arg, 
+                                                &call_interval_mean_arg, 
+                                                &call_duration_mean_arg, 
+                                                &move_interval_mean_arg, 
+                                                &channel_cnt_arg, 
+                                                &num_portables_arg      };
 
     warped::Simulation simulation {"PCS Simulation", argc, argv, cmd_line_args};
 
@@ -214,32 +192,18 @@ int main(int argc, const char **argv) {
     num_cells_y         = num_cells_y_arg.getValue();
     call_interval_mean  = call_interval_mean_arg.getValue();
     call_duration_mean  = call_duration_mean_arg.getValue();
+    move_interval_mean  = move_interval_mean_arg.getValue();
     channel_cnt         = channel_cnt_arg.getValue();
     num_portables       = num_portables_arg.getValue();
 
     std::vector<PcsCell> objects;
-    unsigned int pid = 0;
-    std::shared_ptr<MLCG> rng = std::make_shared<MLCG> ();
-
     for (unsigned int i = 0; i < num_cells_x * num_cells_y; i++) {
 
-        std::vector<std::shared_ptr<Portable>> portables;
-
-        NegativeExpntl interval_expo(call_interval_mean, rng.get());
-        NegativeExpntl duration_expo(call_duration_mean, rng.get());
-
-        for (unsigned int i = 0; i < num_portables; i++) {
-            pid++;
-            auto interval = (unsigned int) interval_expo() + NEG_EXPL_OFFSET;
-            auto duration = (unsigned int) duration_expo() + NEG_EXPL_OFFSET;
-            auto portable = std::make_shared<Portable> (pid, false, 0, interval, duration);
-            portables.push_back(portable);
-        }
-        std::string name = std::string("Object ") + std::to_string(i);
-        objects.emplace_back(name, num_cells_x, num_cells_y, 
-                                        channel_cnt, portables, i);
+        std::string name = std::string("Cell_") + std::to_string(i);
+        objects.emplace_back(   name, num_cells_x, num_cells_y, channel_cnt, 
+                                call_interval_mean, call_duration_mean, 
+                                move_interval_mean, num_portables, i );
     }
-    std::move(rng);
 
     std::vector<warped::SimulationObject*> object_pointers;
     for (auto& o : objects) {
--- models/pcs/pcs_sim.hpp
+++ models/pcs/pcs_sim.hpp
@@ -11,52 +11,9 @@
 #include "NegExp.h"
 
 
-class Portable {
-public:
-
-    Portable(   unsigned int pid, 
-                bool is_busy, 
-                unsigned int call_arrival_ts, 
-                unsigned int call_interval, 
-                unsigned int call_duration      )
-        :   pid_(pid), 
-            is_busy_(is_busy), 
-            call_arrival_ts_(call_arrival_ts), 
-            call_interval_(call_interval), 
-            call_duration_(call_duration) {}
-
-    unsigned int    pid_;
-    bool            is_busy_;
-    unsigned int    call_arrival_ts_;
-    unsigned int    call_interval_;
-    unsigned int    call_duration_;
-};
-
 WARPED_DEFINE_OBJECT_STATE_STRUCT(PcsState) {
 
-    PcsState() = default;
-
-    PcsState(const PcsState& other) {
-
-        idle_channel_cnt_ = other.idle_channel_cnt_;
-
-        portables_ = std::make_shared<std::map <unsigned int, std::shared_ptr<Portable>>>();
-        for (auto it = other.portables_->begin(); it != other.portables_->end(); it++) {
-            auto portable = it->second;
-            auto new_portable = std::make_shared<Portable>( portable->pid_,
-                                                            portable->is_busy_,
-                                                            portable->call_arrival_ts_,
-                                                            portable->call_interval_,
-                                                            portable->call_duration_    );
-            assert(new_portable != nullptr);
-            portables_->insert(portables_->begin(), 
-                std::pair <unsigned int, std::shared_ptr<Portable>> (portable->pid_, 
-                                                                            new_portable));
-        }
-    };
-
     unsigned int idle_channel_cnt_;
-    std::shared_ptr<std::map <unsigned int, std::shared_ptr<Portable>>> portables_;
 };
 
 enum event_type_t {
@@ -80,66 +37,58 @@ public:
 
     PcsEvent() = default;
 
-    PcsEvent(const std::string receiver_name, unsigned int timestamp, 
-                        std::shared_ptr<Portable> portable, event_type_t event_type)
-        : receiver_name_(receiver_name), event_timestamp_(timestamp), event_type_(event_type) {
-
-        assert (portable != nullptr);
-        pid_                = portable->pid_;
-        is_busy_            = portable->is_busy_;
-        call_arrival_ts_    = portable->call_arrival_ts_;
-        call_interval_      = portable->call_interval_;
-        call_duration_      = portable->call_duration_;
-    }
+    PcsEvent(   const std::string   receiver_name, 
+                unsigned int        timestamp, 
+                unsigned int        call_arrival_ts, 
+                event_type_t        event_type         )
+        :   receiver_name_(receiver_name), 
+            event_timestamp_(timestamp), 
+            call_arrival_ts_(call_arrival_ts), 
+            event_type_(event_type) {}
 
     const std::string& receiverName() const { return receiver_name_; }
     unsigned int timestamp() const { return event_timestamp_; }
 
     std::string     receiver_name_;
     unsigned int    event_timestamp_;
-    event_type_t    event_type_;
-    unsigned int    pid_;
-    bool            is_busy_;
     unsigned int    call_arrival_ts_;
-    unsigned int    call_interval_;
-    unsigned int    call_duration_;
+    event_type_t    event_type_;
 
     WARPED_REGISTER_SERIALIZABLE_MEMBERS(cereal::base_class<warped::Event>(this), 
                                             receiver_name_, event_timestamp_, event_type_, 
-                                            pid_, is_busy_, call_arrival_ts_, call_interval_, 
-                                            call_duration_)
+                                            call_arrival_ts_)
 };
 
 class PcsCell : public warped::SimulationObject {
 public:
 
-    PcsCell(    const std::string& name, 
-                unsigned int num_cells_x, 
-                unsigned int num_cells_y, 
-                unsigned int channel_cnt, 
-                std::vector<std::shared_ptr<Portable>> portables, 
-                unsigned int index      )
+    PcsCell(    const std::string&  name, 
+                unsigned int        num_cells_x, 
+                unsigned int        num_cells_y, 
+                unsigned int        channel_cnt, 
+                unsigned int        call_interval_mean, 
+                unsigned int        call_duration_mean, 
+                unsigned int        move_interval_mean, 
+                unsigned int        portable_cnt, 
+                unsigned int        index      )
 
-        :   SimulationObject(name),
-            state_(),
+        :   SimulationObject(name), 
+            state_(), 
             num_cells_x_(num_cells_x), 
-            num_cells_y_(num_cells_y),
-            channel_cnt_(channel_cnt),
-            index_(index),
+            num_cells_y_(num_cells_y), 
+            channel_cnt_(channel_cnt), 
+            call_interval_mean_(call_interval_mean), 
+            call_duration_mean_(call_duration_mean), 
+            move_interval_mean_(move_interval_mean),
+            portable_init_cnt_(portable_cnt),
+            index_(index), 
             rng_(new MLCG) {
 
         // Update the state variables
-        state_ = std::make_shared<PcsState>();
-        state_->idle_channel_cnt_ = channel_cnt_;
-        state_->portables_ = 
-            std::make_shared<std::map <unsigned int, std::shared_ptr<Portable>>>();
-        for (auto& portable : portables) {
-            state_->portables_->insert(state_->portables_->begin(), 
-                std::pair <unsigned int, std::shared_ptr<Portable>> (portable->pid_, portable));
-        }
+        state_.idle_channel_cnt_ = channel_cnt_;
     }
 
-    virtual warped::ObjectState& getState() { return *state_; }
+    virtual warped::ObjectState& getState() { return state_; }
 
     virtual std::vector<std::shared_ptr<warped::Event> > createInitialEvents();
 
@@ -147,12 +96,16 @@ public:
 
 protected:
 
-    std::shared_ptr<PcsState>   state_;
-    unsigned int                num_cells_x_;
-    unsigned int                num_cells_y_;
-    unsigned int                channel_cnt_;
-    unsigned int                index_;
-    std::shared_ptr<MLCG>       rng_;
+    PcsState                state_;
+    unsigned int            num_cells_x_;
+    unsigned int            num_cells_y_;
+    unsigned int            channel_cnt_;
+    unsigned int            call_interval_mean_;
+    unsigned int            call_duration_mean_;
+    unsigned int            move_interval_mean_;
+    unsigned int            portable_init_cnt_;
+    unsigned int            index_;
+    std::shared_ptr<MLCG>   rng_;
 
     std::string compute_move(direction_t direction);
     std::string random_move();
