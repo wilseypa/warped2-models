59a55693e0dd78208b15837576cedc037af515db
Sounak Gupta <guptask@mail.uc.edu>
2015-04-08 18:14:53 -0400

    modified the bulk simulation script and config for cluster mode using mpich

--- scripts/timewarpConfigSimulate.sh
+++ scripts/timewarpConfigSimulate.sh
@@ -11,21 +11,19 @@ function run {
     testCycles=$1
     timeoutPeriod=$2
     model=$3
-    modelBinary=$4
-    modelConfig=$5
-    maxSimTime=$6
-    workerThreads=$7
-    schedulerCount=$8
+    modelCmd=$4
+    maxSimTime=$5
+    workerThreads=$6
+    schedulerCount=$7
 
     for ((iteration=1; iteration <= $testCycles; ++iteration));
     do
         cd ../models/$model/
-        outMsg="\n($iteration/$testCycles) $model $modelConfig : $workerThreads threads, \
-                                                                $schedulerCount schedulers"
+        outMsg="\n($iteration/$testCycles) $modelCmd : $workerThreads threads, \
+                                            $schedulerCount schedulers, max sim time: $maxSimTime"
         echo -e $outMsg
-        runCommand="./$modelBinary $modelConfig --max-sim-time $maxSimTime \
-                    --simulation-type time-warp --time-warp-worker-threads $workerThreads \
-                    --time-warp-scheduler-count $schedulerCount"
+        runCommand="$modelCmd --max-sim-time $maxSimTime --simulation-type time-warp \
+            --time-warp-worker-threads $workerThreads --time-warp-scheduler-count $schedulerCount"
         grepMe=`timeout $timeoutPeriod bash -c "$runCommand" | grep "Simulation completed in "`
         runTime=`echo $grepMe | sed -e 's/.*in \(.*\) second.*/\1/'`
         rollbacks=`echo $grepMe | sed -e 's/.*with \(.*\) rollback.*/\1/'`
@@ -33,7 +31,7 @@ function run {
         cd ../../scripts/
 
         # Write to log file
-        echo "$model,$modelConfig,$workerThreads,$schedulerCount,$runTime,$rollbacks" >> $logFile
+        echo "$model,$modelCmd,$workerThreads,$schedulerCount,$runTime,$rollbacks" >> $logFile
 
         sleep 10
     done
@@ -45,11 +43,11 @@ logFile="logs/$hostname---$date.csv"
 
 # Write csv header
 ## Simulation Threads ScheduleQCount CausalityType Runtime Rollbacks
-echo "Model,ModelConfig,WorkerThreadCount,SchedulerCount,Runtime,Rollbacks" > $logFile
+echo "Model,ModelCommand,WorkerThreadCount,SchedulerCount,Runtime,Rollbacks" > $logFile
 
 trap control_c SIGINT
 
 . $1
 
-#run 10 5 epidemic epidemic_sim "-m model_5500_obj.xml" 100000 4 2
+#run 10 5 epidemic "./epidemic_sim -m model_5500_obj.xml" 100000 4 2
 
