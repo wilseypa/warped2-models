d8d600dc38372cb5b34e849579b72e0725e40ba8
Sounak Gupta <guptask@mail.uc.edu>
2015-05-05 23:33:27 -0400

    removed iscas

--- models/iscas/events.cpp
+++
@@ -1,7 +0,0 @@
-#include "events.hpp"
-#include "warped.hpp"
-
-
-//unfortunately, the cereal serialization library doesn't support registering classes in a header file
-WARPED_REGISTER_POLYMORPHIC_SERIALIZABLE_CLASS(SignalEvent)
-WARPED_REGISTER_POLYMORPHIC_SERIALIZABLE_CLASS(ClockEvent)
