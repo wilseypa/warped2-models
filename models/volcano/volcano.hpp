225caa294b97afba17992df02f0324691088fb5f
Sounak Gupta <guptask@mail.uc.edu>
2016-03-14 14:05:13 -0400

    changes to the volcano header

--- models/volcano/volcano.hpp
+++ models/volcano/volcano.hpp
@@ -12,8 +12,10 @@
 
 WARPED_DEFINE_LP_STATE_STRUCT(VolcanoState) {
 
-    unsigned int num_particles_;
-    //velocity of each particle
+    // Velocity vector - V1_x,V1_y,V1_z;V2_x,V2_y,V2_z,...
+    // There can be multiple particles in an LP at the same instant
+    // Note: We assume there are no particle collisions in this model
+    std::string particle_velocity_;
 };
 
 enum volcano_event_t {
@@ -27,12 +29,14 @@ class VolcanoEvent : public warped::Event {
 public:
     VolcanoEvent() = default;
     VolcanoEvent(   const std::string& receiver_name, 
-                    const volcano_event_t type,
-                    const unsigned int vel_x,
-                    const unsigned int vel_y,
-                    const unsigned int vel_z,
+                    const volcano_event_t type, 
+                    const unsigned int vel_x, 
+                    const unsigned int vel_y, 
+                    const unsigned int vel_z, 
                     const unsigned int timestamp    )
-        : receiver_name_(receiver_name), type_(type), vel_x_(vel_x), vel_y_(vel_y), vel_z_(vel_z), ts_(timestamp) {}
+        :   receiver_name_(receiver_name), type_(type), 
+            vel_x_(vel_x), vel_y_(vel_y), vel_z_(vel_z), 
+            ts_(timestamp) {}
 
     const std::string& receiverName() const { return receiver_name_; }
     unsigned int timestamp() const { return ts_; }
@@ -44,7 +48,8 @@ public:
     unsigned int vel_z_;
     unsigned int ts_;
 
-    WARPED_REGISTER_SERIALIZABLE_MEMBERS(cereal::base_class<warped::Event>(this), receiver_name_, type_, vel_x_, vel_y_, vel_z_, ts_)
+    WARPED_REGISTER_SERIALIZABLE_MEMBERS(cereal::base_class<warped::Event>(this), 
+                                receiver_name_, type_, vel_x_, vel_y_, vel_z_, ts_)
 };
 
 class Volcano : public warped::LogicalProcess {
@@ -66,13 +71,9 @@ public:
 protected:
     std::shared_ptr<std::default_random_engine> rng_;
     const unsigned int index_;
-    const unsigned int num_particles_;
-    const unsigned int grid_max_x_;
-    const unsigned int grid_max_y_;
-    const unsigned int grid_max_z_;
-    const unsigned int blast_origin_x_;
-    const unsigned int blast_origin_y_;
-    const unsigned int blast_origin_z_;
+    const unsigned int position_x_;
+    const unsigned int position_y_;
+    const unsigned int position_z_;
 };
 
 #endif
