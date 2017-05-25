71ea9fca58ee5bfef5d1d4ecc5f3b8aac71cf70a
Caleb van Haaren <vanhaacf@mail.uc.edu>
2017-05-25 11:09:47 -0400

    Changes made to header formatting, suggested by Sounak

--- models/forest/forest.hpp
+++ models/forest/forest.hpp
@@ -10,103 +10,111 @@
 #include "warped.hpp"
 
 
+/* Status of the fire */
+enum status_t {
+
+    UNBURNT,
+    GROWTH,
+    DECAY,
+    BURNT_OUT
+};
+
 WARPED_DEFINE_LP_STATE_STRUCT(ForestState) {
-   enum burning_status_{
-      UNBURNT,
-      GROWTH,
-      DECAY,
-      BURNT_OUT
-   }; 
 
-   unsigned int heat_content_;
+    status_t burn_status;
+    unsigned int heat_content_;
 };
 
+/*Forest fire events */
 enum forest_event_t {
-        RADIATION_IN,
-        RADIATION_OUT,
-        IGNITION,
-        PEAK  
+
+    RADIATION_TIMER,
+    RADIATION,
+    IGNITION,
+    PEAK  
 };
 
-enum direction_t {  //The direction of the spread of fire from the currently burning LP
+/*The direction of the spread of fire from the currently burning LP*/
+enum direction_t {  
 
     NORTH,
-    NORTH_WEST,
     NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
     SOUTH,
     SOUTH_WEST,
-    SOUTH_EAST,
-    EAST,
     WEST,
+    NORTH_WEST
 };
 
-class ForestEvent : public warped::Event {  //Class Definition of a forest fire event
-    public:
-        ForestEvent() = default;
-        ForestEvent(const std::string& receiver_name, const forest_event_t type,
-                                                        const unsigned int timestamp)
-            : receiver_name_(receiver_name), type_(type), ts_(timestamp) {} //Initializing Class Components
+/* Class Definition of a forest fire event */
+class ForestEvent : public warped::Event {
+public:
+    ForestEvent() = default;
+    ForestEvent(const std::string& receiver_name,
+                const forest_event_t type,
+                const unsigned int heat_content,
+                const unsigned int timestamp)
+        : receiver_name_(receiver_name), type_(type), ts_(timestamp) {}
         
-        const std::string& receiverName() const { return receiver_name_; }
-        unsigned int timestamp() const { return ts_; }
+    const std::string& receiverName() const { return receiver_name_; }
+    unsigned int timestamp() const { return ts_; }
 
-        std::string receiver_name_;
-        forest_event_t type_;
-        unsigned int ts_;
+    std::string receiver_name_;
+    forest_event_t type_;
+    unsigned int heat_content_;
+    unsigned int ts_;
 
-        WARPED_REGISTER_SERIALIZABLE_MEMBERS(cereal::base_class<warped::Event>(this), receiver_name_, type_, ts_)
-
+    WARPED_REGISTER_SERIALIZABLE_MEMBERS(cereal::base_class<warped::Event>(this), 
+                                            receiver_name_, type_, heat_content_, ts_)
 };
 
 class Forest : public warped::LogicalProcess{
-    public:
-        Forest( const std::string& name,
-                const unsigned int size_x,    
-                const unsigned int size_y,    
-                const unsigned int ignition_threshold,
-                const unsigned int heat_rate,
-                const unsigned int peak_threshold,
-                const unsigned int radiation_percent,
-                const unsigned int burnout_threshold,
-                const unsigned int index)
+public:
+    Forest( const std::string& name,
+            const unsigned int size_x,    
+            const unsigned int size_y,    
+            const unsigned int ignition_threshold,
+            const unsigned int heat_rate,
+            const unsigned int peak_threshold,
+            const unsigned int radiation_percent,
+            const unsigned int burnout_threshold,
+            const unsigned int index    )
                 
-        :   LogicalProcess(name),
-            state_(),
-            rng_(new std::default_random_engine(index)),
-            size_x_(size_x),    
-            size_y_(size_y),   
-            ignition_threshold_(ignition_threshold),
-            peak_threshold_(peak_threshold),
-            radiation_percent_(radiation_percent),
-            burnout_threshold_(burnout_threshold),
-            index_(index){
-                state_.burning_status_ = UNBURNT;
-                heat_content_ = 0;
-            }
-            
-            virtual std::vector<std::shared_ptr<warped::Event> > initializeLP() override;
-            virtual std::vector<std::shared_ptr<warped::Event> > receiveEvent(const warped::Event&);
-            virtual warped::LPState& getState() { return this->state_; }
-            
-            ForestState state_;
-            
-            static inline std::string lp_name(const unsigned int);
-            
-            
-            
-            
-    protected:
-        std::shared_ptr<std::default_random_engine> rng_;
-        const unsigned int size_x_;   //the x coordinate of the LP on the picture
-        const unsigned int size_y_;    //the y coordinate of the LP on the picture
-        const unsigned int ignition_threshold_; // threshold of heat that the LP needs to ignite
-        const unsigned int peak_threshold_; //threshold of heat reached by the fire before it stops growing
-        const unsigned int radiation_percent_; //Percent of heat radiation released at each event
-        const unsigned int burnout_threshold_; //Threshold of Heat that the lp needs to reach to burn out
-        const unsigned int index_; //The identifier used by the model to distinguish between LPs
-        
-        std::string compute_spread(); //Function to Spread the heat to adjacent cells
+    :   LogicalProcess(name),
+        state_(),
+        rng_(new std::default_random_engine(index)),
+        size_x_(size_x),    
+        size_y_(size_y),   
+        ignition_threshold_(ignition_threshold),
+        peak_threshold_(peak_threshold),
+        radiation_percent_(radiation_percent),
+        burnout_threshold_(burnout_threshold),
+        index_(index) {
 
+        /* Initialize the state variables */
+        state_.burning_status_ = UNBURNT;
+        state_.heat_content_ = 0;
+    }
+            
+    virtual std::vector<std::shared_ptr<warped::Event> > initializeLP() override;
+    virtual std::vector<std::shared_ptr<warped::Event> > receiveEvent(const warped::Event&);
+    virtual warped::LPState& getState() { return this->state_; }
+            
+    ForestState state_;
+    static inline std::string lp_name(const unsigned int);
+
+protected:
+    std::shared_ptr<std::default_random_engine> rng_;
+    const unsigned int size_x_;   //The width of the picture being used
+    const unsigned int size_y_;    //The height of the picture being used
+    const unsigned int ignition_threshold_; // threshold of heat that the LP needs to ignite
+    const unsigned int peak_threshold_; //threshold of heat reached by the fire before it stops growing
+    const unsigned int radiation_percent_; //Percent of heat radiation released at each event
+    const unsigned int burnout_threshold_; //Threshold of Heat that the lp needs to reach to burn out
+    const unsigned int index_; //The identifier used by the model to distinguish between LPs
+ 
+    std::string compute_spread(); //Function to Spread the heat to adjacent cells
 }
 
 #endif
