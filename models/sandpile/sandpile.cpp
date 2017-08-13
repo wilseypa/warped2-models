127e78a55ad9f426b6f08a5b88e0897ffc54b3a5
Sounak Gupta <guptask@mail.uc.edu>
2017-08-13 02:34:42 -0400

    recified the error - now only grid center can add sand

--- models/sandpile/sandpile.cpp
+++ models/sandpile/sandpile.cpp
@@ -24,20 +24,24 @@ std::vector<std::shared_ptr<warped::Event> > Vertex::initializeLP() {
     /* If there is no grain of sand */
     if (!state_.sandpile_height_) return events;
 
-    /* If sandpile is about to topple */
+    /* Sandpile can't initially be at topple threshold */
+    assert (state_.sandpile_height_ < 4);
+
+    /* Add a grain of sand and send an update event to itself */
+    state_.sandpile_height_++;
+    events.emplace_back( new SandEvent {node_name(this->index_), TS_INTERVAL} );
+
+    /* If the sandpile is at topple threshold */
     if (state_.sandpile_height_ == 4) {
+
         state_.sandpile_height_ = 0;
         state_.collapse_cnt_++;
-        return events;
 
-    } else if (state_.sandpile_height_ == 3) { /* If the sandpile is unstable */
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, UP))   , TS_INTERVAL} );
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, RIGHT)), TS_INTERVAL} );
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, DOWN)) , TS_INTERVAL} );
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, LEFT)) , TS_INTERVAL} );
     }
-    state_.sandpile_height_++;
-    events.emplace_back( new SandEvent {node_name(this->index_), TS_INTERVAL} );
 
     return events;
 }
@@ -49,20 +53,27 @@ std::vector<std::shared_ptr<warped::Event> > Vertex::receiveEvent(const warped::
 
     std::vector<std::shared_ptr<warped::Event>> events;
 
-    /* If sandpile is about to topple */
-    if (state_.sandpile_height_ == 4) {
-        state_.sandpile_height_ = (sand_event.receiver_name_ == node_name(this->index_)) ? 0 : 1;
-        state_.collapse_cnt_++;
-        return events;
+    /* Add a grain of sand */
+    state_.sandpile_height_++;
 
-    } else if (state_.sandpile_height_ == 3) { /* If the sandpile is unstable */
+    /*  NOTE:   Only the vertex (here center of grid) has non-empty sandpile initially.
+                It can keep getting more sand.
+     */
+    if (sand_event.receiver_name_ == node_name(this->index_)) {
+        events.emplace_back( new SandEvent {node_name(this->index_), event_ts} );
+    }
+
+    /* If the sandpile is at topple threshold */
+    if (state_.sandpile_height_ == 4) {
+
+        state_.sandpile_height_ = 0;
+        state_.collapse_cnt_++;
+
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, UP))   , event_ts} );
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, RIGHT)), event_ts} );
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, DOWN)) , event_ts} );
         events.emplace_back( new SandEvent {node_name(neighbor(this->index_, LEFT)) , event_ts} );
     }
-    state_.sandpile_height_++;
-    events.emplace_back( new SandEvent {node_name(this->index_), event_ts} );
 
     return events;
 }
@@ -139,36 +150,29 @@ int main(int argc, const char **argv) {
     /* Post-processing : Print fractal image of the grid */
     auto grid = new ppm(grid_dimension, grid_dimension);
 
-    unsigned int sand_cnt = 0;
     for (auto& lp : lps) {
         auto i = lp.index_;
-        sand_cnt += lp.state_.sandpile_height_;
-        if (lp.state_.sandpile_height_ == 4) { /* Vertex will topple immediately, mark WHITE */
+        if (lp.state_.sandpile_height_ == 3) { /* Vertex will topple immediately, mark WHITE */
             grid->r[i] = 255;
             grid->g[i] = 255;
             grid->b[i] = 255;
 
-        } else if (lp.state_.sandpile_height_ == 3) { /* Vertex will topple after WHITE, mark RED */
+        } else if (lp.state_.sandpile_height_ == 2) { /* Vertex will topple after WHITE, mark RED */
             grid->r[i] = 255;
             grid->g[i] = 0;
             grid->b[i] = 0;
 
-        } else if (lp.state_.sandpile_height_ == 2) { /* Vertex will topple after RED, mark YELLOW */
+        } else if (lp.state_.sandpile_height_ == 1) { /* Vertex will topple after RED, mark YELLOW */
             grid->r[i] = 255;
             grid->g[i] = 255;
             grid->b[i] = 0;
 
-        } else if (lp.state_.sandpile_height_ == 1) { /* Vertex will topple after YELLOW, mark GREEN */
-            grid->r[i] = 0;
-            grid->g[i] = 255;
-            grid->b[i] = 0;
-
         } else {
-            /* If the vertex has collapsed before and empty now, mark PINK */
+            /* If the vertex has collapsed before and empty now, mark GREEN */
             if (lp.state_.collapse_cnt_) {
-                grid->r[i] = 255;
-                grid->g[i] = 0;
-                grid->b[i] = 255;
+                grid->r[i] = 0;
+                grid->g[i] = 255;
+                grid->b[i] = 0;
 
             } else { /* If vertex has never collapsed, mark BLACK */
                 grid->r[i] = 0;
@@ -179,8 +183,6 @@ int main(int argc, const char **argv) {
     }
     grid->write("output_grid.ppm");
     delete grid;
-
-    std::cout << "Total amount of sand generated = " << sand_cnt << std::endl;
 
     return 0;
 }
