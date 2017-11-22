321238e6b6dadea6c03665d7a58085a26b0729b7
yangjb1 <yangjb@mail.uc.edu>
2017-11-21 21:17:04 -0500

    fix bug on when menu switches pop up labels are not clear before new labels are pop up

--- models/synthetic/ui/synthetic.py
+++ models/synthetic/ui/synthetic.py
@@ -52,16 +52,16 @@ fp_min.insert(0, "1000,1000")
 # network
 def nw(*args):
     if networkV.get() == 'Watts-Strogatz':
-        mean_degreeL = Label(n, text = 'Mean Degree').grid(row = 1, column = 1)
-        probabilityL = Label(n, text = 'Probability').grid(row = 1, column = 3)
-        mean_degree = Entry(n).grid(row = 1, column = 2)
-        probability = Entry(n).grid(row = 1, column = 4)
+        mean_degreeL.config(text = 'Mean Degree')
+        probabilityL.config(text = 'Probability')
+        mean_degree.grid(row = 1, column = 2)
+        probability.grid(row = 1, column = 4)
 
     elif networkV.get() == 'Barabasi-Albert':
-        mean_degreeL = Label(n, text = '').grid(row = 1, column = 1)
-        probabilityL = Label(n, text = 'Probability').grid(row = 1, column = 3)
-        mean_degree = Entry(n).grid(row = 1, column = 2)
-        probability = Entry(n).grid(row = 1, column = 4)
+        mean_degreeL.config(text = 'Mean Degree')
+        probabilityL.config(text = 'Probability')
+        mean_degree.grid(row = 1, column = 2)
+        probability.grid(row = 1, column = 4)
 
 n = Frame(master)
 n.pack()
@@ -78,64 +78,68 @@ mean_degreeL = Label(n, text = '')
 mean_degreeL.grid(row = 1, column = 1)
 probabilityL = Label(n, text = '')
 probabilityL.grid(row = 1, column = 3)
+mean_degree = Entry(n)
+mean_degree.grid(row = 1, column = 2)
+mean_degree.grid_remove()
+probability = Entry(n)
+probability.grid(row = 1, column = 4)
+probability.grid_remove()
 
 
 # event send distribution
 def es_choice(*args):
     if esV.get() == 'Exponential':
-        es_l1 = Label(es, text = 'Lambda')
-        es_l1.grid(row = 1, column = 1)
-        es_l2 = Label(es, text = 'Ceiling')
-        es_l2.grid(row = 1, column = 3)
-        es_l3 = Label(es, text = '').grid(row = 1, column = 5)
+        es_l1.config(text = 'Lambda')
+        es_l2.config(text = 'Ceiling')
+        es_l3.config(text = '')
         es_v1.grid(row = 1, column = 2)
         es_v2.grid(row = 1, column = 4)
         es_v3.grid_remove()
 
     elif esV.get() == 'Geometric':
-        es_l1 = Label(es, text = 'Probabiity of Success').grid(row = 1, column = 1)
-        es_l2 = Label(es, text = 'Ceiling').grid(row = 1, column = 3)
-        es_l3 = Label(es, text = '').grid(row = 1, column = 5)
+        es_l1.config(text = 'Probabiity of Success')
+        es_l2.config(text = 'Ceiling')
+        es_l3.config(text = '')
         es_v1.grid(row = 1, column = 2)
         es_v2.grid(row = 1, column = 4)
         es_v3.grid_remove()
 
     elif esV.get() == 'Binomial':
-        es_l1 = Label(es, text = 'Probabiity of Success').grid(row = 1, column = 1)
-        es_l2 = Label(es, text = 'Ceiling').grid(row = 1, column = 3)
-        es_l3 = Label(es, text = '').grid(row = 1, column = 5)
+        es_l1.config(text = 'Probabiity of Success')
+        es_l2.config(text = 'Ceiling')
+        es_l3.config(text = '')
         es_v1.grid(row = 1, column = 2)
         es_v2.grid(row = 1, column = 4)
         es_v3.grid_remove()
 
     elif esV.get() == 'Normal':
-        es_l1 = Label(es, text = 'Mean').grid(row = 1, column = 1)
-        es_l2 = Label(es, text = 'Standard Deviation').grid(row = 1, column = 3)
-        es_l3 = Label(es, text = 'Ceiling').grid(row = 1, column = 5)
+        es_l1.config(text = 'Mean')
+        es_l2.config(text = 'Standard Deviation')
+        es_l3.config(text = 'Ceiling')
         es_v1.grid(row = 1, column = 2)
         es_v2.grid(row = 1, column = 4)
         es_v3.grid(row = 1, column = 6)
 
     elif esV.get() == 'Uniform':
-        es_l1 = Label(es, text = 'Upper Bound').grid(row = 1, column = 1)
-        es_l2 = Label(es, text = 'Lower Bound').grid(row = 1, column = 3)
-        es_l3 = Label(es, text = 'Ceiling').grid(row = 1, column = 5)
+        es_l1.config(text = 'Upper Bound')
+        es_l2.config(text = 'Lower Bound')
+        es_l3.config(text = 'Ceiling')
         es_v1.grid(row = 1, column = 2)
         es_v2.grid(row = 1, column = 4)
         es_v3.grid(row = 1, column = 6)
 
     elif esV.get() == 'Poisson':
-        es_l1 = Label(es, text = 'Mean(>3)').grid(row = 1, column = 1)
-        es_l2 = Label(es, text = 'Ceiling').grid(row = 1, column = 3)
-        es_l3 = Label(es, text = '').grid(row = 1, column = 5)
+        es_l1.config(text = 'Mean(>3)')
+        es_l2.config(text = 'Ceiling')
+        es_l3.config(text = '')
         es_v1.grid(row = 1, column = 2)
         es_v2.grid(row = 1, column = 4)
         es_v3.grid_remove()
 
     elif esV.get() == 'Lognormal':
-        es_l1 = Label(es, text = 'Mean').grid(row = 1, column = 1)
-        es_l2 = Label(es, text = 'Standard Deviation').grid(row = 1, column = 3)
-        es_l3 = Label(es, text = 'Ceiling').grid(row = 1, column = 5)
+        es_l1.config(text = 'Mean')
+        es_l2.config(text = 'Standard Deviation')
+        es_l3.config(text = 'Ceiling')
         es_v1.grid(row = 1, column = 2)
         es_v2.grid(row = 1, column = 4)
         es_v3.grid(row = 1, column = 6)
@@ -177,60 +181,46 @@ es_v3.grid_remove()
 # node selection
 def ns_choice(*args):
     if nsV.get() == 'Exponential':
-        ns_l1 = Label(ns, text = 'Lambda').grid(row = 1, column = 1)
-        ns_l2 = Label(ns, text = '').grid(row = 1, column = 3)
-        ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+        ns_l1.config(text = 'Lambda')
+        ns_l2.config(text = '')
         ns_v1.grid(row = 1, column = 2)
         ns_v2.grid_remove()
-        ns_v3.grid_remove()
 
     elif nsV.get() == 'Geometric':
-        ns_l1 = Label(ns, text = 'Probabiity of Success').grid(row = 1, column = 1)
-        ns_l2 = Label(ns, text = '').grid(row = 1, column = 3)
-        ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+        ns_l1.config(text = 'Probabiity of Success')
+        ns_l2.config(text = '')
         ns_v1.grid(row = 1, column = 2)
         ns_v2.grid_remove()
-        ns_v3.grid_remove()
 
     elif nsV.get() == 'Binomial':
-        ns_l1 = Label(ns, text = 'Probabiity of Success').grid(row = 1, column = 1)
-        ns_l2 = Label(ns, text = '').grid(row = 1, column = 3)
-        ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+        ns_l1.config(text = 'Probabiity of Success')
+        ns_l2.config(text = '')
         ns_v1.grid(row = 1, column = 2)
         ns_v2.grid_remove()
-        ns_v3.grid_remove()
 
     elif nsV.get() == 'Normal':
-        ns_l1 = Label(ns, text = 'Mean').grid(row = 1, column = 1)
-        ns_l2 = Label(ns, text = 'Standard Deviation').grid(row = 1, column = 3)
-        ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+        ns_l1.config(text = 'Mean')
+        ns_l2.config(text = 'Standard Deviation')
         ns_v1.grid(row = 1, column = 2)
         ns_v2.grid(row = 1, column = 4)
-        ns_v3.grid_remove()
 
     elif nsV.get() == 'Uniform':
-        ns_l1 = Label(ns, text = 'Upper Bound').grid(row = 1, column = 1)
-        ns_l2 = Label(ns, text = 'Lower Bound').grid(row = 1, column = 3)
-        ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+        ns_l1.config(text = 'Upper Bound')
+        ns_l2.config(text = 'Lower Bound')
         ns_v1.grid(row = 1, column = 2)
         ns_v2.grid(row = 1, column = 4)
-        ns_v3.grid_remove()
 
     elif nsV.get() == 'Poisson':
-        ns_l1 = Label(ns, text = 'Mean(>3)').grid(row = 1, column = 1)
-        ns_l2 = Label(ns, text = '').grid(row = 1, column = 3)
-        ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+        ns_l1.config(text = 'Mean(>3)')
+        ns_l2.config(text = '')
         ns_v1.grid(row = 1, column = 2)
         ns_v2.grid_remove()
-        ns_v3.grid_remove()
 
     elif nsV.get() == 'Lognormal':
-        ns_l1 = Label(ns, text = 'Mean').grid(row = 1, column = 1)
-        ns_l2 = Label(ns, text = 'Standard Deviation').grid(row = 1, column = 3)
-        ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+        ns_l1.config(text = 'Mean')
+        ns_l2.config(text = 'Standard Deviation')
         ns_v1.grid(row = 1, column = 2)
         ns_v2.grid(row = 1, column = 4)
-        ns_v3.grid_remove()
 
 ns = Frame(master)
 ns.pack()
@@ -250,18 +240,16 @@ nsMenu = OptionMenu(ns, nsV, *nsM, command = ns_choice)
 Label(ns, text = 'Select Node to Send Event').grid(row = 0, column = 0)
 nsMenu.grid(row = 1, column = 0)
 
-ns_l1 = Label(ns, text = '').grid(row = 1, column = 1)
-ns_l2 = Label(ns, text = '').grid(row = 1, column = 3)
-ns_l3 = Label(ns, text = '').grid(row = 1, column = 5)
+ns_l1 = Label(ns, text = '')
+ns_l1.grid(row = 1, column = 1)
+ns_l2 = Label(ns, text = '')
+ns_l2.grid(row = 1, column = 3)
 ns_v1 = Entry(ns)
 ns_v2 = Entry(ns)
-ns_v3 = Entry(ns)
 ns_v1.grid(row = 1, column = 2)
 ns_v2.grid(row = 1, column = 4)
-ns_v3.grid(row = 1, column = 6)
 ns_v1.grid_remove()
 ns_v2.grid_remove()
-ns_v3.grid_remove()
 
 # push buttons
 
