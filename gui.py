9ae04fedf9ddfd71f104ae96a9e68fbaebcd8313
yangjb1 <yangjb@mail.uc.edu>
2017-12-18 19:02:22 -0500

    added scrollable feature to gui when gui is too long, and readability to user enter entry

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from Tkinter import *
+from Tix import *
 import tkMessageBox
 import subprocess
 import random
@@ -19,16 +20,26 @@ del models[0:n+1]
 
 def button():
     # run config
-    print check_list
+
+    for entry in entries:
+        print entry.get()
+
+
+'''
+    for check in checks:
+        #print check.get()
+        print check
+'''
+
 
 def conf_choice():
+    top = Toplevel()
     global label
     label = []
     entry_check = []
     explian = []
     tmp_str = ''
 
-    top = Toplevel()
 
     tmpfile = '/tmp/help' + str(random.randint(0,100))
     run_command = './models/' +  model_var.get() + '/' + model_var.get() + '_sim --help > ' + tmpfile
@@ -45,33 +56,32 @@ def conf_choice():
                     read_status = True
                 continue
 
-            else:
-                entry_flag = False
-                # filter out the white spaces
-                if not lines.strip(): continue
+            entry_flag = False
+            # filter out the white spaces
+            if not lines.strip(): continue
 
-                for words in lines.split():
-                    # find the argument
-                    if '--' in words:
-                        entry_flag = True
-                        explian_flag += 1
-                        label.append(words)
-                        if explian_flag:
-                            explian.append(tmp_str)
-                            tmp_str = ''
+            for words in lines.split():
+            # find the argument
+                if '--' in words:
+                    entry_flag = True
+                    explian_flag += 1
+                    label.append(words)
+                    if explian_flag:
+                        explian.append(tmp_str)
+                        tmp_str = ''
 
                 # entry variable attach to arg exist or not
-                if entry_flag:
-                    if '<' in lines:
-                        entry_check.append(True)
-                        #print words
-                    else:
-                        entry_check.append(False)
+            if entry_flag:
+                if '<' in lines:
+                    entry_check.append(True)
+                #print words
+                else:
+                    entry_check.append(False)
 
-                # ignore argument line only save explaination
-                if '   -' not in lines:
-                    for words in lines.split():
-                        tmp_str += words + ' '
+            # ignore argument line only save explaination
+            if '   -' not in lines:
+                for words in lines.split():
+                    tmp_str += words + ' '
 
         '''
         print len(label)
@@ -82,21 +92,43 @@ def conf_choice():
         print explian
         '''
 
-        global check_list
-        check_list = [IntVar]*len(label)
+        #global check_list
+        #check_list = [IntVar]*len(label)
         #label_list = [None]*len(label)
-        entry_list = [None]*len(label)
-        button_list = [None]*len(label)
-        check = [IntVar]*len(label)
+        #entry_list = [None]*len(label)
+        #button_list = [None]*len(label)
+        #check = [IntVar]*len(label)
+        scroll = ScrolledWindow(top)
+        scroll.pack()
+        win = scroll.window
+
+        global checks
+        global entries
+        checks = [IntVar()]
+        entries = []
+
+        check = IntVar()
+
+        '''
+        checks = IntVar()
+        entries = StringVar()
+        '''
 
         for i in range(0, len(label)):
-            check[i] = Checkbutton(top).grid(row = i, column = 0)
+            #check = Checkbutton(top, variable = checks[i]).grid(row = i, column = 0)
+            check = Checkbutton(win)
+            check.grid(row = i, column = 0)
+            #check.get()
             #print check[i].get()
-            Label(top, text = label[i]).grid(row = i, column = 1)
-            entry_list[i] = Entry(top).grid(row = i, column = 2)
-            #button_list[i] = Button(top, text = '?').grid(row = i, column = 3)
+            checks.append(check)
+            Label(win, text = label[i]).grid(row = i, column = 1)
+            #entry_list[i] = Entry(top).grid(row = i, column = 2)
+            entry = Entry(win)
+            entry.grid(row = i, column = 2)
+            entries.append(entry)
+            Button(win, text = '?').grid(row = i, column = 3)
 
-            config = Button(top, text = 'Run', command = button).grid(row = len(label)+1, column = 1)
+            config = Button(win, text = 'Run', command = button).grid(row = len(label)+1, column = 1)
 
 
 
@@ -112,7 +144,7 @@ model_var = StringVar()
 
 model_var.set(models[0])
 
-modelMenu = OptionMenu(master, model_var, *models)
+modelMenu = Tkinter.OptionMenu(master, model_var, *models)
 Label(master, text = 'Choose the Simulation Model\t').pack(side = LEFT)
 modelMenu.pack(side = LEFT)
 
