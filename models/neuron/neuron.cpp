88746d8bf6f44a48d476bb4053a892d615bc3ece
Sounak Gupta <guptask@mail.uc.edu>
2018-03-03 21:50:39 -0500

    made changes to event handler and argument list for addition of neural refractory time

--- models/neuron/neuron.cpp
+++ models/neuron/neuron.cpp
@@ -1,4 +1,4 @@
-/* Implementation of a Neural Network Simulation */
+/* Simulation of a neural network - read the header and README for further documentation */
 
 #include "neuron.hpp"
 #include <fstream>
@@ -18,22 +18,36 @@ std::vector<std::shared_ptr<warped::Event> > Cell::receiveEvent(const warped::Ev
     std::vector<std::shared_ptr<warped::Event> > response_events;
     auto received_event = static_cast<const Spike&>(event);
 
-    // Add details of the paper
-    double delta_t = received_event.timestamp() - this->state_.latest_update_ts_;
-    auto it = this->neighbors_.find(event.sender_name_);
-    assert(it != this->neighbors_.end());
+    /* Check whether it is a refractory self-event */
+    if (received_event.sender_name_ == received_event.receiverName()) {
 
-    this->state_.membrane_potential_ *= exp(-delta_t/membrane_time_const_);
-    this->state_.membrane_potential_ += it->second;
-    this->state_.latest_update_ts_ = received_event.timestamp();
+        this->state_.membrane_potential_    = 0;
+        this->state_.latest_update_ts_      = received_event.timestamp();
 
-    if (this->state_.membrane_potential_ >= 1.0) {
-        // Send event to neighboring neurons
-        this->state_.membrane_potential_ = 0; 
+    } else if (this->state_.membrane_potential_ < 1.0) { /* Check if the neuron is responsive */
+
+        /* IntFire1 : Basic Integrate and Fire Model */
+        auto it = this->neighbors_.find(event.sender_name_);
+        assert(it != this->neighbors_.end());
+
+        double delta_t = received_event.timestamp() - this->state_.latest_update_ts_;
+        this->state_.membrane_potential_ *= exp(-delta_t/membrane_time_const_);
+        this->state_.membrane_potential_ += it->second;
+        this->state_.latest_update_ts_ = received_event.timestamp();
+
+        if (this->state_.membrane_potential_ >= 1.0) {
+
+            this->state_.membrane_potential_ = 2.0;
+            this->state_.num_spikes_++;
+
+            unsigned int ts = received_event.timestamp() + this->refractory_period_;
+
+            /* Send the refractory self-event with receive time = ts */
+            response_events.emplace_back(new Spike {this->name_, ts});
+
+            /* TODO: Send spike events to all its connected neighbors with receive time = ts */
+        }
     }
-
-    // Send refractory event
-
     return response_events;
 }
 
@@ -42,7 +56,8 @@ int main(int argc, const char** argv) {
     /* Read the command line arguments for study name and connection threshold */
     std::string study_name          = "ADHD200_CC200";
     double connection_threshold     = 0.0;
-    double membrane_time_const      = 10.0; /* Unit is milli seconds */
+    double membrane_time_const      = 10.0; /* Unit is msecs */
+    unsigned int refractory_period  = 5;    /* Unit is msecs */
 
     TCLAP::ValueArg<std::string> study_name_arg( "", "study-name",
                                     "Name of the study", false, study_name, "string");
@@ -53,9 +68,13 @@ int main(int argc, const char** argv) {
     TCLAP::ValueArg<double> membrane_time_const_arg( "", "membrane-time-const",
             "Membrane Time Constant (in milli seconds)", false, membrane_time_const, "double");
 
+    TCLAP::ValueArg<unsigned int> refractory_period_arg( "", "refractory-period",
+            "Refractory Period (in milli seconds)", false, refractory_period, "unsigned int");
+
     std::vector<TCLAP::Arg*> args = {   &study_name_arg,
                                         &connection_threshold_arg,
-                                        &membrane_time_const_arg
+                                        &membrane_time_const_arg,
+                                        &refractory_period_arg
                                     };
 
     warped::Simulation neuron_sim {"Neural Network Simulation", argc, argv, args};
@@ -63,6 +82,7 @@ int main(int argc, const char** argv) {
     study_name              = study_name_arg.getValue();
     connection_threshold    = connection_threshold_arg.getValue();
     membrane_time_const     = membrane_time_const_arg.getValue();
+    refractory_period       = refractory_period_arg.getValue();
 
     /* Read the abbreviated file for neuron names and create the LPs */
     std::ifstream file_stream;
@@ -72,11 +92,9 @@ int main(int argc, const char** argv) {
         std::cerr << "Invalid names file - " << buffer << std::endl;
         return 0;
     }
-    unsigned int neuron_count = 0;
     std::vector<Cell> lps;
     while (getline(file_stream, buffer)) {
-        lps.emplace_back(buffer, membrane_time_const);
-        neuron_count++;
+        lps.emplace_back(buffer, membrane_time_const, refractory_period);
     }
     file_stream.close();
 
@@ -91,9 +109,15 @@ int main(int argc, const char** argv) {
     while (getline(file_stream, buffer)) {
         size_t pos = 0;
         unsigned int col_index = 0;
+
         while ((pos = buffer.find_first_of(" ", 0)) != std::string::npos) {
+
             std::string::size_type sz;
             auto weight = std::stod(buffer.substr(0, pos), &sz);
+
+            /* Note: Negative weight indicates inhibitory neurons
+                     Positive weight indicates excitatory neurons
+             */
             if ((row_index != col_index) && (weight > connection_threshold)) {
                 lps[row_index].addNeighbor(lps[col_index].name_, weight);
             }
@@ -103,6 +127,23 @@ int main(int argc, const char** argv) {
         row_index++;
     }
     file_stream.close();
+
+    /* Simulate the neuron model */
+    std::vector<warped::LogicalProcess*> lp_pointers;
+    for (auto& lp : lps) {
+        lp_pointers.push_back(&lp);
+    }
+    neuron_sim.simulate(lp_pointers);
+
+    /* Post-simulation statistics */
+    unsigned int num_active_neurons = 0, num_spikes = 0;
+    for (auto& lp : lps) {
+        num_active_neurons += lp.spikeCount() ? 1 : 0;
+        num_spikes += lp.spikeCount();
+    }
+    std::cout   << num_active_neurons << " out of " << lps.size()
+                << " neurons fired a total of " << num_spikes << " spikes."
+                << std::endl;
 
     return 0;
 }
--- models/neuron/neuron.hpp
+++ models/neuron/neuron.hpp
@@ -1,3 +1,7 @@
+/*  This model simulated how signals flow across a network of neurons. It has been modelled
+    using details from the following paper:
+      - https://www.neuron.yale.edu/neuron/static/papers/discrete/discrete_event_poster.pdf
+ */
 #ifndef NEURON_HPP
 #define NEURON_HPP
 
@@ -11,6 +15,7 @@ WARPED_DEFINE_LP_STATE_STRUCT(CellState) {
 
     double membrane_potential_;
     unsigned int latest_update_ts_;
+    unsigned int num_spikes_;
 };
 
 class Spike : public warped::Event {
@@ -35,25 +40,33 @@ class Cell : public warped::LogicalProcess {
 public:
 
     /* NOTE : It has been assumed that the neuron config files don't have repetitive names */
-    Cell( const std::string& name, double membrane_time_const )
+    Cell(   const std::string& name,
+            double membrane_time_const,
+            unsigned int refractory_period  )
+
         :   LogicalProcess(name),
             membrane_time_const_(membrane_time_const),
+            refractory_period_(refractory_period),
             state_() {
 
         state_.membrane_potential_  = 0;
         state_.latest_update_ts_    = 0;
+        state_.num_spikes_          = 0;
     }
 
     virtual std::vector<std::shared_ptr<warped::Event> > initializeLP() override;
+
     virtual std::vector<std::shared_ptr<warped::Event> > receiveEvent(const warped::Event&);
+
     virtual warped::LPState& getState() { return this->state_; }
 
-    void addNeighbor( std::string name, double weight ) {
-        neighbors_.emplace(name, weight);
-    }
+    unsigned int spikeCount() { return this->state_.num_spikes_; }
+
+    void addNeighbor( std::string name, double weight ) { neighbors_.emplace(name, weight); }
 
 protected:
-    double membrane_time_const_ = 0;
+    double membrane_time_const_     = 0;
+    unsigned int refractory_period_ = 0;
     std::unordered_map<std::string, double> neighbors_;
     CellState state_;
 };
