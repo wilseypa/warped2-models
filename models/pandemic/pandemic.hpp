abe051354843a0f2b032009cc05c8345e87b21ba
Vivek Mehra <vivek.1376@gmail.com>
2020-08-21 02:46:22 -0400

    Revert "- partial bugfixes"
    This reverts commit 1a53ad3b2cbd517b536b08333db1be1debd1a313.

--- models/pandemic/pandemic.hpp
+++ models/pandemic/pandemic.hpp
@@ -322,6 +322,9 @@ public:
 
     void readConfig(const std::string& fname, std::vector<Location>& lps) {
 
+        std::unique_ptr<jsoncons::json_cursor> cur_ (new jsoncons::json_cursor(new jsoncons::json_cursor(
+                                                                                   new std::ifstream(fname))));
+        
         std::ifstream is(fname);
 
         jsoncons::json input_data = jsoncons::json::parse(is);
@@ -395,7 +398,7 @@ public:
         jsoncons::json jsontowrite;
 
         jsoncons::json disease_model(jsoncons::json_object_arg, {
-                {"date_date", jsoncons::null_type()},
+                {"date_date", jsoncons::json::null},
                 {"diffusion_trig_interval_in_hrs", CONFIG->diffusion_trig_interval_in_hrs},
                 {"mean_incubation_duration_in_days", CONFIG->mean_incubation_duration_ / TIME_UNITS_IN_DAY},
                 {"mean_infection_duration_in_days", CONFIG->mean_infection_duration_ / TIME_UNITS_IN_DAY},
