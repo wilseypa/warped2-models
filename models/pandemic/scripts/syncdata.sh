68590df660a01c7ccea18c149c9c6b0cd59a676a
Vivek Mehra <mehravr@mail.uc.edu>
2020-12-03 05:20:30 +0000

    Auto upload line chart plots to OneDrive
    Add change to OneDrive cloud sync script to auto-upload plots created by line
    charting webapp to OneDrive

--- models/pandemic/scripts/syncdata.sh
+++ models/pandemic/scripts/syncdata.sh
@@ -7,6 +7,8 @@ while true; do
              "${REMOTE_NAME}:${REMOTE_RESULTS_BASEDIR_PATH}/${REMOTE_SIMRESULTS_DIRNAME}" && \
           rclone --quiet --checksum sync "${LOCAL_RESULTS_BASEDIR_PATH}/${LOCAL_LOGS_DIRNAME}" \
                  "${REMOTE_NAME}:${REMOTE_RESULTS_BASEDIR_PATH}/${REMOTE_LOGS_DIRNAME}" && \
+          rclone --quiet --checksum sync "${LOCAL_RESULTS_BASEDIR_PATH}/${LOCAL_PLOTS_RELATIVE_PATH}" \
+                 "${REMOTE_NAME}:${REMOTE_RESULTS_BASEDIR_PATH}/${REMOTE_PLOTS_DIRNAME}" && \
           [[ $? == 0 ]] && echo "synced successfully" || echo "error"
 
       echo "sleeping ..."
