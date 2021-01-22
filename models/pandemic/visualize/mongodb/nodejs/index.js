416d268d45fd0423cae635c07169143a55b2b657
presleyswilliam <presleyswilliam@gmail.com>
2021-01-22 00:34:39 -0500

    adding more console.logs

--- models/pandemic/visualize/mongodb/nodejs/index.js
+++ models/pandemic/visualize/mongodb/nodejs/index.js
@@ -60,7 +60,8 @@ app.get('/isDevEnv', (request, response) => {
 });
 
 app.post('/callSimulate', (req, res) => {
-  console.log(typeof(req.body));
+  console.log(typeof(JSON.stringify(req.body)));
+  console.log(JSON.stringify(req.body));
   request.post({
 	//headers: {'content-type' : 'application/x-www-form-urlencoded'},
 	url:     'http://localhost:8082/simulate',
