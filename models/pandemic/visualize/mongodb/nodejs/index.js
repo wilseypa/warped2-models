bf3e3ee2b31d179ddc5b6b7e64ecfde5eacafbcd
presleyswilliam <presleyswilliam@gmail.com>
2021-01-22 00:08:34 -0500

    syntax error...

--- models/pandemic/visualize/mongodb/nodejs/index.js
+++ models/pandemic/visualize/mongodb/nodejs/index.js
@@ -61,7 +61,7 @@ app.post('/callSimulate', (req, res) => {
   console.log(req.body)
   request.post({
 	headers: {'content-type' : 'application/x-www-form-urlencoded'},
-	url:     'http://localhost:8082/getstatus',
+	url:     'http://localhost:8082/simulate',
 	body:    req.body
   }, function(error, response, bodyRes){
 	console.log(bodyRes);
