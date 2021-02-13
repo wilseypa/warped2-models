6ed59c3a27764994205f8c6414cbc8b997a5dea7
presleyswilliam <presleyswilliam@gmail.com>
2021-02-13 15:54:02 -0500

    dynamic ranges temporarily

--- models/pandemic/visualize/js/visualization.js
+++ models/pandemic/visualize/js/visualization.js
@@ -1022,20 +1022,20 @@ function padsRePlot(covidStats, pressNum){
             highestPercentage = tempHighestPercentage;
         }
     }
-    // for (i = 0; i < percentageArray.length; i++) {   // DYNAMIC LEGEND RANGE
-    //     percentageArray[i] = ((highestPercentage / percentageArray.length) * i).toFixed(2);
-    // }
-    if (pressNum == 0) { // August1 Simulated
-        percentageArray = [0, 0.7, 1.4, 2.1, 2.8, 3.5, 4.2, 4.9, 5.6];
-    } else if (pressNum == 1) { // August1 Actual
-        percentageArray = [0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12];
-    } else if (pressNum == 2) { // September15 Simulated
-        percentageArray = [0, 0.7, 1.4, 2.1, 2.8, 3.5, 4.2, 4.9, 5.6];
-    } else if (pressNum == 3) { // September15 Actual
-        percentageArray = [0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12];
-    } else {
-        console.log("PADS Plots completed");
+    for (i = 0; i < percentageArray.length; i++) {   // DYNAMIC LEGEND RANGE
+        percentageArray[i] = ((highestPercentage / percentageArray.length) * i).toFixed(2);
     }
+    // if (pressNum == 0) { // August1 Simulated
+    //     percentageArray = [0, 0.7, 1.4, 2.1, 2.8, 3.5, 4.2, 4.9, 5.6];
+    // } else if (pressNum == 1) { // August1 Actual
+    //     percentageArray = [0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12];
+    // } else if (pressNum == 2) { // September15 Simulated
+    //     percentageArray = [0, 0.7, 1.4, 2.1, 2.8, 3.5, 4.2, 4.9, 5.6];
+    // } else if (pressNum == 3) { // September15 Actual
+    //     percentageArray = [0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12];
+    // } else {
+    //     console.log("PADS Plots completed");
+    // }
     
     for (i = 0; i < percentageArray.length; i++) {
         let selectString = "#legendValue" + i;
