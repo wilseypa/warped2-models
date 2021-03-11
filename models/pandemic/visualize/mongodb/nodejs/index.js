b46bcf83b1a885af197e28bc5d9d3b7c0bc33175
presleyswilliam <presleyswilliam@gmail.com>
2021-03-10 23:42:01 -0500

    reverting after testing

--- models/pandemic/visualize/mongodb/nodejs/index.js
+++ models/pandemic/visualize/mongodb/nodejs/index.js
@@ -168,9 +168,9 @@ app.post('/callSimulate', (req, res) => {
 
 	const options = {
 		method: 'post',
-		url: 'http://ec2-3-22-85-65.us-east-2.compute.amazonaws.com:7096/simulate',
+		url: 'http://localhost:8082/simulate',
 		headers: {'Content-Type': 'application/json;charset=UTF-8'},
-		auth: {username: "scooby", password: "doobyd00"},
+		// auth: {username: "scooby", password: "doobyd00"},
 		data: {
 			"jobid":"xshe7i9ieg2iy90yrb9ow934",
 			"start_date":{"value":"07-01-2020"},
@@ -210,9 +210,9 @@ app.get('/callGetstatus', (req, res) => {
 	// });
 	const options = {
 		method: 'get',
-		url: 'http://ec2-3-22-85-65.us-east-2.compute.amazonaws.com:7096/getstatus',
+		url: 'http://localhost:8082/getstatus',
 		headers: {'Content-Type': 'application/json;charset=UTF-8'},
-		auth: {username: "scooby", password: "doobyd00"},
+		// auth: {username: "scooby", password: "doobyd00"},
 		data: {
 			"jobid":"xshe7i9ieg2iy90yrb9ow934"
 		}//,
