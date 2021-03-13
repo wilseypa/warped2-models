ac2a65c4af6e122f52ad2b5b4df23b13f63ccbbf
presleyswilliam <presleyswilliam@gmail.com>
2021-03-13 00:41:10 -0500

    removing console.logs

--- models/pandemic/visualize/mongodb/nodejs/index.js
+++ models/pandemic/visualize/mongodb/nodejs/index.js
@@ -133,8 +133,8 @@ app.get('/isDevEnv', (request, response) => {
 });
 
 app.post('/callSimulate', (req, res) => {
-  console.log(typeof(JSON.stringify(req.body)));
-  console.log(JSON.stringify(req.body));
+//   console.log(typeof(JSON.stringify(req.body)));
+//   console.log(JSON.stringify(req.body));
 
 	// const options = {
 	// 	url: 'http://localhost:8082/simulate',
