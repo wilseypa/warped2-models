db70d2b9e3cfc32c93a053bfe03a8886418f6f8c
presleyswilliam <presleyswilliam@gmail.com>
2021-03-22 22:29:01 -0400

    removing comments

--- models/pandemic/visualize/js/miscFuncs.js
+++ models/pandemic/visualize/js/miscFuncs.js
@@ -29,32 +29,6 @@ function formatDate(dateValue, dateFormat, dateType) {
 
 }
 
-// function minimizeMapControlsContent(elId, durationOfAnimation) { //INPUTS MUST BE STRINGS
-//     document.getElementById(elId).style.animation = "minimizeMapControlsContent " + durationOfAnimation + "s";
-//     document.getElementById(elId).style.animationFillMode = "forwards";
-//     setTimeout(function(){ document.getElementById(elId).style.display = "none"; }, durationOfAnimation*1000);
-// }
-// function maximizeMapControlContent(elId, durationOfAnimation, delayOfAnimation) { //INPUTS MUST BE STRINGS
-//     document.getElementById(elId).style.visibility = "hidden";
-//     document.getElementById(elId).style.display = "block";
-//     document.getElementById(elId).style.animation = "maximizeMapControlsContent " + durationOfAnimation + "s";
-//     document.getElementById(elId).style.animationFillMode = "forwards";
-//     document.getElementById(elId).style.animationDelay = delayOfAnimation + "s";
-// }
-
-// function fadeOutDisplay(elId, durationOfAnimation) { //INPUTS MUST BE STRINGS
-//     document.getElementById(elId).style.animation = "fadeOutDisplay " + durationOfAnimation + "s";
-//     document.getElementById(elId).style.animationFillMode = "forwards";
-//     setTimeout(function(){ document.getElementById(elId).style.display = "none"; }, durationOfAnimation*1000);
-// }
-// function fadeInDisplay(elId, durationOfAnimation, delayOfAnimation) { //INPUTS MUST BE STRINGS
-//     document.getElementById(elId).style.visibility = "hidden";
-//     document.getElementById(elId).style.display = "block";
-//     document.getElementById(elId).style.animation = "fadeInDisplay " + durationOfAnimation + "s";
-//     document.getElementById(elId).style.animationFillMode = "forwards";
-//     document.getElementById(elId).style.animationDelay = delayOfAnimation + "s";
-// }
-
 function createLoadingBar() {
     var loadingElHandle = document.createElement("div");
     loadingElHandle.setAttribute("id", "loadingBarHandle");
