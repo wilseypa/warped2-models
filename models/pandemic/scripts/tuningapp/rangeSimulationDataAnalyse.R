b0b25e92f6871a5303c954cddae2caf546680e72
Vivek Mehra <vivek.1376@gmail.com>
2022-02-05 12:36:00 +0530

    update

--- models/pandemic/scripts/tuningapp/rangeSimulationDataAnalyse.R
+++ models/pandemic/scripts/tuningapp/rangeSimulationDataAnalyse.R
@@ -50,6 +50,64 @@ US_fips_county_name_df$FIPS <- as.character(US_fips_county_name_df$FIPS)
 
 dirs_to_plot <- grep('.+', list.dirs("./plotSourceData/", full.names = FALSE), value=TRUE)
 
+
+
+# first, create transmissiblity chart
+
+suppressWarnings(rm(tblity_arr))
+suppressWarnings(rm(tblitydata))
+suppressWarnings(rm(row))
+suppressWarnings(rm(newrow))
+
+jsondata <- fromJSON("./plotSourceData/metrics")
+tblity_arr <- jsondata$paramsUsed$disease_model$transmissibility
+
+actualUSdata <- read.csv("./plotSourceData/US/actual.csv", header = TRUE)
+xf2 <- c("transmissibility")
+
+newrow <- data.frame(Date=as.Date(character()),
+                     variable=factor(levels=xf2),
+                     value=double())
+
+tblitydata <- data.frame(Date=as.Date(character()),
+                         variable=factor(levels=xf2),
+                         value=double())
+
+for(i in 1:nrow(actualUSdata)) {
+  row <- actualUSdata[i,]
+  newrow[1,]$Date <- row$Date
+  
+  if (length(tblity_arr) == 1) {
+    t_expval <- tblity_arr[1] * exp(-1 * i)
+    newtval <- t_expval
+  } else if (i > length(tblity_arr)) {
+    newtval <- tblity_arr[length(tblity_arr)]
+  } else {
+    newtval <- tblity_arr[i]
+  }
+  
+  newrow[1,]$value <- newtval
+  newrow[1,]$variable <- "transmissibility"
+  
+  tblitydata <- rbind(tblitydata, newrow)
+}
+
+pt2 <- ggplot(tblitydata, aes(x=Date,y=value,color=variable)) +
+  geom_line(size=0.8) + 
+  scale_color_manual(values = c("transmissibility" = "#568c7e")) +
+  scale_y_log10() +
+  labs(y="transmissibility (log scale)") +
+  theme_light() +
+  theme(aspect.ratio = 1, legend.position="none", text = element_text(size=9)) +
+  scale_x_date(breaks=c(min(newdata$Date), max(newdata$Date)), 
+               minor_breaks = list_date_breaks,
+               date_labels = "%d %b %Y")
+
+
+ggsave(paste("../../plotImages/", currSimDir, "/", plotname, ".svg", sep=''), width = 6, height = 6, units = "in", plot=pt1)
+ggsave(paste("../../plotImages/", currSimDir, "/", "transmissibility_curve.svg", sep=''), width = 6, height = 6, units = "in", plot=pt2)
+
+
 # plot disease values in each data directory
 for (dir in dirs_to_plot) {
   print(paste("plotting", dir, "..."))
@@ -148,51 +206,6 @@ for (dir in dirs_to_plot) {
     currdate <- currdate + 30
   }
   
-  
-  # now, create transmissiblity chart
-  
-  suppressWarnings(rm(tblity_arr))
-  suppressWarnings(rm(tblitydata))
-  suppressWarnings(rm(row))
-  suppressWarnings(rm(newrow))
-  
-  jsondata <- fromJSON(paste("./plotSourceData/", "metrics", sep=''))
-  tblity_arr <- jsondata$paramsUsed$disease_model$transmissibility
-  
-  
-  xf2 <- c("transmissibility")
-  
-  newrow <- data.frame(Date=as.Date(character()),
-                       variable=factor(levels=xf2),
-                       value=double())
-  
-  tblitydata <- data.frame(Date=as.Date(character()),
-                           variable=factor(levels=xf2),
-                           value=double())
-  
-  for(i in 1:nrow(actual)) {
-    row <- actual[i,]
-    newrow[1,]$Date <- row$Date
-    
-    if (length(tblity_arr) == 1) {
-      t_expval <- tblity_arr[1] * exp(-1 * i)
-      newtval <- t_expval
-    } else if (i > length(tblity_arr)) {
-      newtval <- tblity_arr[length(tblity_arr)]
-    } else {
-      newtval <- tblity_arr[i]
-    }
-    
-    newrow[1,]$value <- newtval
-    newrow[1,]$variable <- "transmissibility"
-    
-    tblitydata <- rbind(tblitydata, newrow)
-  }
-  
-  
-
-  
-  
   pt1 <- ggplot(newdata, aes(x=Date, y=value, colour=Disease_metric, 
                 linetype=Disease_metric)) +
          geom_line(size=0.6) +
@@ -205,23 +218,9 @@ for (dir in dirs_to_plot) {
          scale_x_date(breaks=c(min(newdata$Date), max(newdata$Date)), 
                       minor_breaks = list_date_breaks,
                       date_labels = "%d %b %Y")
-
-  pt2 <- ggplot(tblitydata, aes(x=Date,y=value,color=variable)) +
-         geom_line(size=0.8) + 
-         scale_color_manual(values = c("transmissibility" = "568c7e")) +
-         scale_y_log10() +
-         labs(y="transmissibility (log scale)") +
-         theme_light() +
-         theme(aspect.ratio = 1, legend.position="none", text = element_text(size=9)) +
-         scale_x_date(breaks=c(min(newdata$Date), max(newdata$Date)), 
-                      minor_breaks = list_date_breaks,
-                      date_labels = "%d %b %Y")
-  
   
   ggsave(paste("../../plotImages/", currSimDir, "/", plotname, ".pdf", sep=''), width = 6, height = 6, units = "in", plot=pt1)
-  ggsave(paste("../../plotImages/", currSimDir, "/", plotname, ".svg", sep=''), width = 6, height = 6, units = "in", plot=pt1)
   ggsave(paste("../../plotImages/", currSimDir, "/", "transmissibility_curve.pdf", sep=''), width = 6, height = 6, units = "in", plot=pt2)
-  ggsave(paste("../../plotImages/", currSimDir, "/", "transmissibility_curve.svg", sep=''), width = 6, height = 6, units = "in", plot=pt2)
 }
 
 
